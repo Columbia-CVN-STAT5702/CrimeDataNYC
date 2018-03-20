aabe0ad557f27d6d260441e7b42aae3a5561e8ae
Rich Lavery <rjl2168@columbia.edu>
2018-03-19 21:30:29 -0400

    added full moon data

--- Code/rjl2168.r
+++ Code/rjl2168.r
@@ -31,6 +31,17 @@ weather_data$TMAX <- as.numeric(weather_data$TMAX)
 #Merge the data together
 crime_df <- crime_df[weather_data, on=.(DateStart = DATE)]
 
+###############################################################################
+#Read in moon phase data
+moon_data <- fread("nyc_moon_data.csv", na.strings="", select = c("date", "phase"), stringsAsFactors = FALSE)
+moon_data$date <- as.Date(moon_data$date, format='%m/%d/%Y')
+moon_data$phase <- as.factor(moon_data$phase)
+full_moon_data <- moon_data %>% filter(phase == "Full Moon")
+
+#Merge the moon phase data into the main data frame
+crime_df <- crime_df %>% left_join(moon_data, by = c("DateStart" = "date"))
+
+#Generate plot to compare average number of crimes on rainy days as compared to non-rainy days
 rainy_day_summary <- crime_df %>% filter(PRCP > 0.00) %>% group_by(Pct) %>% summarize(count_rain = n(), n_days_rain = n_distinct(DateStart), Rain = count_rain/n_days_rain) %>% drop_na() %>% select(Pct, Rain)
 no_rain_summary <- crime_df %>% filter(PRCP == 0.00) %>% group_by(Pct) %>% summarize(count_no_rain = n(), n_days_no_rain = n_distinct(DateStart), NoRain = count_no_rain/n_days_no_rain) %>% drop_na() %>% select(Pct, NoRain)
 #Merge the two summary tables together
@@ -40,6 +51,19 @@ weather_summary$Pct <- as.factor(weather_summary$Pct)
 tidy_weather_summary <- weather_summary %>% gather(key = "Weather", value = "AvgDailyReports", -Pct)
 #Plot precincts on rainy days
 ggplot(tidy_weather_summary, aes(x = Pct, y = AvgDailyReports, color = Weather)) + geom_point() + coord_flip() + ggtitle("Affect of Precipitation on Crime Reports")
+
+#Generate plot to compare averge number of crimes on full moon days as compared to non-full moon days
+crime_summary_reg <- crime_df %>% filter(phase != "Full Moon") %>% group_by(Pct) %>% summarize(count_reg = n(), n_days_reg = n_distinct(DateStart), Avg_Reg = count_reg/n_days_reg) %>% drop_na() %>% select(Pct, Avg_Reg)
+crime_summary_fm <- crime_df %>% filter(phase == "Full Moon") %>% group_by(Pct) %>% summarize(count_fm = n(), n_days_fm = n_distinct(DateStart), Avg_Fm = count_fm/n_days_fm) %>% drop_na() %>% select(Pct, Avg_Fm)
+#Merge the summary tables together
+fm_summary <- crime_summary_reg %>% left_join(crime_summary_fm, by = c("Pct" = "Pct"))
+fm_summary$Pct <- as.factor(fm_summary$Pct)
+#Tidy the summary table
+tidy_fm_summary <- fm_summary %>% gather(key = "Moon Phase", value = "AvgDailyReports", -Pct)
+#Cleveland dot plot to compare crime rate on full moon days
+ggplot(tidy_fm_summary, aes(x = Pct, y = AvgDailyReports, color = `Moon Phase`)) + geom_point() + coord_flip() + ggtitle("Affect of Full Moon on Crime Reports")
+
+
 
 #Plot histogram of incidents by police precinct
 data_pct <- crime_df %>% group_by(Pct) %>% summarize(count = n()) %>% drop_na()
@@ -61,3 +85,4 @@ start_date <- as.Date("2012-01-01")
 stop_date <- as.Date("2012-10-31")
 data3 <- crime_df %>% filter(DateReport >= start_date, DateReport <= stop_date) %>% select(DateReport, Level) %>% drop_na() %>% mutate(month_year = format(DateReport, "%m/%Y")) %>% group_by(month_year, Level) %>% summarize(total = n())
 ggplot(data3, aes(x = month_year, y = total)) + geom_col() + facet_grid(~Level) + coord_flip()
+
