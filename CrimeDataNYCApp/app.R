4d349df0c75bc8d7cf5f9cd45145fd4f52aab651
Rich Lavery <rich.lavery@zebra.com>
2018-03-29 19:05:23 -0400

    Started app development

--- CrimeDataNYCApp/app.R
+++ CrimeDataNYCApp/app.R
@@ -3,41 +3,50 @@
 # the STAT W5701 Final Project
 
 library(shiny)
+library(leaflet)
+library(dplyr)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
    
    # Application title
-   titlePanel("Old Faithful Geyser Data"),
+   titlePanel("NYC Crime Data Visualization"),
    
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         sliderInput("bins",
-                     "Number of bins:",
-                     min = 1,
-                     max = 50,
-                     value = 30)
+         dateInput("startDate", "Start Date"),
+         dateInput("endDate", "End Date"),
+         selectInput("Pct", "Precinct", choices = c("1", "104"), "1")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
-         plotOutput("distPlot")
+         leafletOutput("NycMap"),
+         p(),
+         actionButton("updateMap", "Show Points")
       )
    )
 )
 
-# Define server logic required to draw a histogram
+#Read in the NycAppData1.csv file for use in the app
+appData <- fread("../Data_Files/NycAppData1.csv", na.strings="", stringsAsFactors = TRUE)
+#Convert start date to the proper date format
+appData$DateStart <- as.Date(appData$DateStart, format='%m/%d/%Y')
+
+#Server Function
 server <- function(input, output) {
-   
-   output$distPlot <- renderPlot({
-      # generate bins based on input$bins from ui.R
-      x    <- faithful[, 2] 
-      bins <- seq(min(x), max(x), length.out = input$bins + 1)
-      
-      # draw the histogram with the specified number of bins
-      hist(x, breaks = bins, col = 'darkgray', border = 'white')
-   })
+  points <- eventReactive(input$updateMap, {
+    data_filter <- appData %>% filter(DateStart >= input$startDate, DateStart <= input$endDate) %>% select("Lat", "Long")
+   }, ignoreNULL = FALSE)
+  
+  output$NycMap <- renderLeaflet({
+    leaflet() %>%
+      addProviderTiles(providers$Stamen.TonerLite,
+                       options = providerTileOptions(noWrap = TRUE)
+      ) %>%
+      addMarkers(data = as.numeric(points())
+  })
 }
 
 # Run the application 
