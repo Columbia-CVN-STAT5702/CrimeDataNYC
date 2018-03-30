53b1bf563a58d9e2c3e14488d619715d8599ed9a
Rich Lavery <rjl2168@columbia.edu>
2018-03-30 06:39:36 -0400

    updates to the app

--- CrimeDataNYCApp/app.R
+++ CrimeDataNYCApp/app.R
@@ -16,16 +16,15 @@ ui <- fluidPage(
    # Sidebar with a slider input for number of bins 
    sidebarLayout(
       sidebarPanel(
-         dateInput("startDate", "Start Date", "06-01-2012"),
-         selectInput("boro", "Borough", choices = c("ALL", "MANHATTAN", "BROOKLYN", "QUEENS", "BRONX", "STATEN ISLAND"), "ALL"),
-         selectInput("Pct", "Precinct", choices = c("1", "104"), "1")
+         dateInput("startDate", "Date", value = "06-01-2012", min = "01-01-2006", max = "12-31-16"),
+         selectInput("boro", "Borough", choices = c("ALL", "MANHATTAN", "BROOKLYN", "QUEENS", "BRONX", "STATEN ISLAND"), "ALL")
       ),
       
       # Show a plot of the generated distribution
       mainPanel(
          leafletOutput("NycMap"),
          p(),
-         actionButton("updateMap", "Show Points")
+         actionButton("updateMap", "Update Points")
       )
    )
 )
@@ -38,20 +37,39 @@ server <- function(input, output) {
   
   points <- eventReactive(input$updateMap, {
     if (input$boro == "ALL"){
-      filtered_data <- NycAppData %>% filter(DateStart == input$startDate) %>% select("Lat", "Long") %>% drop_na()
+      filtered_data <- NycAppData %>% filter(DateStart == input$startDate) %>% drop_na()
     }
     else {
-      filtered_data <- NycAppData %>% filter(DateStart == input$startDate) %>% filter(Boro == input$boro) %>% 
-        select("Lat", "Long") %>% drop_na()
+      filtered_data <- NycAppData %>% filter(DateStart == input$startDate) %>% filter(Boro == input$boro) %>% drop_na()
     }
-    cbind(filtered_data$Long, filtered_data$Lat)
+    #cbind(filtered_data$Long, filtered_data$Lat)
+    filtered_data
    }, ignoreNULL = FALSE)
   
   output$NycMap <- renderLeaflet({
-    leaflet() %>%
-      addProviderTiles(providers$Stamen.TonerLite,
-                       options = providerTileOptions(noWrap = TRUE)) %>%
-      addMarkers(data = points())
+    
+    labs <- lapply(seq(nrow(points())), function(i) {
+      paste0( '<p>', points()[i, "Level"], '<p></p>', 
+              points()[i, "Pct"], ', ', 
+              points()[i, "OffenseDesc"],'</p><p>' ) 
+    })
+    
+    leaflet(data = points()) %>%
+      addProviderTiles(providers$Stamen.TonerLite, options = providerTileOptions(noWrap = TRUE)) %>%
+      addMarkers(
+        lng = ~Long, 
+        lat = ~Lat, 
+        label = lapply(labs, HTML),
+        #label = ~as.character(OffenseDesc),
+        labelOptions = labelOptions(direction = "bottom",
+                                    style = list(
+                                      "color" = "red",
+                                      "font-family" = "serif",
+                                      "font-style" = "italic",
+                                      "box-shadow" = "3px 3px rgba(0,0,0,0.25)",
+                                      "font-size" = "14px",
+                                      "border-color" = "rgba(0,0,0,0.5)")),
+        clusterOptions = markerClusterOptions())
   })
 }
 
